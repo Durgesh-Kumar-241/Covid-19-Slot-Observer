ecbda86abaa49b1f17030caf7c53ea20098c9788
Durgesh <kdurgesh029@gmail.com>
2021-09-19 22:48:33 +0530

    pp

--- app/src/main/java/com/dktechhub/mnnit/ee/statussaver/Threader.java
+++ app/src/main/java/com/dktechhub/mnnit/ee/statussaver/Threader.java
@@ -19,13 +19,12 @@ import java.util.TimeZone;
 
 public class Threader extends Thread{
     public boolean isCancelled = false;
-    private String date;
     int disttID=-1,pin_code=-1;
-    boolean pin_code_mode=true;
-    private String link="";
-    private int dose=1;
-    private int min_age_lim;
-    private Caller caller;
+    boolean pin_code_mode;
+    private final String link;
+    private final int dose;
+    private final int min_age_lim;
+    private final Caller caller;
 
     public Threader(Context context,Caller caller) {
         super();
@@ -36,18 +35,18 @@ public class Threader extends Thread{
         min_age_lim=sharedPreferences.getInt("min_age_lim",18);
         SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
         sdf.setTimeZone(TimeZone.getDefault());
-        date= sdf.format(new Date());
-        Log.d("Theader",date);
+        String date = sdf.format(new Date());
+        Log.d("Theader", date);
 
 
         if(pin_code_mode)
         {
             pin_code=sharedPreferences.getInt("pin_code",-1);
-            link = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode="+pin_code+"&date="+date;
+            link = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode="+pin_code+"&date="+ date;
 
         }else {
             disttID=sharedPreferences.getInt("disttID",-1);
-            link="https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id="+disttID+"&date="+date;
+            link="https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id="+disttID+"&date="+ date;
         }
 
         Log.d("Threader","age "+min_age_lim+" dose "+dose+" pin "+pin_code+" pin code mode "+pin_code_mode);
@@ -89,15 +88,13 @@ public class Threader extends Thread{
 
 
     public  void getSlotdetailsfetch()
-    {    //String link = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=277001&date=13-09-2021";
-
+    {
         try{    URL url = new URL(link);
             HttpURLConnection con2 =(HttpURLConnection) url.openConnection();
             con2.setRequestMethod("GET");
             con2.setRequestProperty("accept","application/json, text/plain, */*");
             con2.setRequestProperty("accept-encoding","gzip, deflate, br");
             con2.setRequestProperty("accept-language","en-US,en;q=0.9");
-            //con2.setRequestProperty("authorization","Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiIxNGMxNTVkOS1mZDU5LTQwN2ItOWIyMC05NWY1MDEzZjI3NmYiLCJ1c2VyX2lkIjoiMTRjMTU1ZDktZmQ1OS00MDdiLTliMjAtOTVmNTAxM2YyNzZmIiwidXNlcl90eXBlIjoiQkVORUZJQ0lBUlkiLCJtb2JpbGVfbnVtYmVyIjo5NjE2MjgwMDg4LCJiZW5lZmljaWFyeV9yZWZlcmVuY2VfaWQiOjUzNDE4NDY0Mjc4NjUwLCJzZWNyZXRfa2V5IjoiYjVjYWIxNjctNzk3Ny00ZGYxLTgwMjctYTYzYWExNDRmMDRlIiwic291cmNlIjoiY293aW4iLCJ1YSI6Ik1vemlsbGEvNS4wIChXaW5kb3dzIE5UIDEwLjA7IFdpbjY0OyB4NjQpIEFwcGxlV2ViS2l0LzUzNy4zNiAoS0hUTUwsIGxpa2UgR2Vja28pIENocm9tZS85Mi4wLjQ1MTUuMTU5IFNhZmFyaS81MzcuMzYgRWRnLzkyLjAuOTAyLjc4IiwiZGF0ZV9tb2RpZmllZCI6IjIwMjEtMDktMTFUMDg6MDA6MTUuOTUwWiIsImlhdCI6MTYzMTM0NzIxNSwiZXhwIjoxNjMxMzQ4MTE1fQ.IHL1gBCAxxuL5wqIwl53qndNKYe4Njqm9ZOf4TMNBBc");
             con2.setRequestProperty("origin","https://selfregistration.cowin.gov.in");
             con2.setRequestProperty("referer","https://selfregistration.cowin.gov.in/");
             con2.setRequestProperty("sec-ch-ua","Chromium\";v=\"92\", \" Not A;Brand\";v=\"99\", \"Microsoft Edge\";v=\"92\"");
@@ -107,9 +104,7 @@ public class Threader extends Thread{
             con2.setRequestProperty("sec-fetch-site","cross-site");
             con2.setRequestProperty("user-agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 Edg/92.0.902.78");
             p("con2: "+con2.getResponseCode());
-            //p(con2.getHeaderFields().toString());
-            // p(con2.getContent().toString());
-            if(con2.getResponseCode()==200)
+           if(con2.getResponseCode()==200)
             {
                 try(BufferedReader br=new BufferedReader(new InputStreamReader(con2.getInputStream())))
                 {
@@ -120,13 +115,8 @@ public class Threader extends Thread{
                         sb.append(s.trim());
                     }
 
-                    //p("respoce data: "+sb.toString());
-                    parse(sb.toString());
-                    //String t = sb.toString();
-                    //token = t.substring(t.indexOf("\":\"")+3,t.indexOf("\","));
-                    //p("Congo OTP VERIFIED");
-                    //p("token: "+token);
-                }
+                     parse(sb.toString());
+                  }
             }
 
         }   catch(Exception e)
@@ -173,11 +163,6 @@ public class Threader extends Thread{
     public void p(String m)
     {
         Log.d("Threader",m);
-    }
-
-    public void p(int m)
-    {
-        Log.d("Threader", String.valueOf(m));
     }
 
     public interface Caller{
